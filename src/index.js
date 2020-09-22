60e5313bb91e07cf4e72cb5254d08e2b0c65454f
takuya shirakane <d113169@gmail.com>
2020-09-22 18:39:22 +0900

    implements events index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
 import reducer from './reducers'
+import EventsIdnex from './components/events-idnex';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <EventsIdnex />
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
